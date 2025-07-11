26ebfa9bf001cff7013c5f96225998164eec54db
bannzai <star.kojiki@gmail.com>
2025-07-12 00:34:03 +0900

    Initial Commit

---
+++ KatahabaCamera/KatahabaCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  KatahabaCameraApp.swift
+//  KatahabaCamera
+//
+//  Created by bannzai on 2025/07/12.
+//
+
+import SwiftUI
+
+@main
+struct KatahabaCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
