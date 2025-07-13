9291d841070d7f80a9e4eb67fd3b9442348b510c
bannzai <star.kojiki@gmail.com>
2025-07-13 15:35:19 +0900

    Remove private access-control

--- KatahabaCamera/Camera/CameraViewModel.swift
+++ KatahabaCamera/Camera/CameraViewModel.swift
@@ -26,8 +26,8 @@ class CameraViewModel: ObservableObject {
   private let shoulderDetector = ShoulderDetector()
   private let imageWarper = ImageWarper()
   
-  private var displaySize: CGSize = .zero
-  private var imageSize: CGSize = .zero
+  var displaySize: CGSize = .zero
+  var imageSize: CGSize = .zero
   private var detectedFaceRect: CGRect?
   private var imageDisplayScale: CGFloat = 1.0
   private var displayOffset: CGPoint = .zero
